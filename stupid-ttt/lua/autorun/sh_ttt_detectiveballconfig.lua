6ac8c826fd2d192f4f13300681f904791b6b463b
Sir Francis Billard <sirfrancisbillard@gmail.com>
2017-04-25 08:09:23 -1000

    Update sh_ttt_detectiveballconfig.lua

--- stupid-ttt/lua/autorun/sh_ttt_detectiveballconfig.lua
+++ stupid-ttt/lua/autorun/sh_ttt_detectiveballconfig.lua
@@ -3,22 +3,11 @@ AddCSLuaFile()
 DetectiveBallConfig = {}
  
 DetectiveBallConfig.PZ = true
-
 DetectiveBallConfig.TouchInfect = false
-
 DetectiveBallConfig.ScreenTick = true
-
 DetectiveBallConfig.TInfect = false
-
 DetectiveBallConfig.Silent = false
-
 DetectiveBallConfig.InfectMessage = true
-
 DetectiveBallConfig.InfectTimeMin = 10
-
 DetectiveBallConfig.InfectTimeMax = 10
-
 DetectiveBallConfig.ScreenTickFreq = 0.2
-
-
-
