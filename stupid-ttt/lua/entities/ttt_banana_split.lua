7eb5e18ae46c723dc02443b025f01b5ff2b11a74
Sir Francis Billard <sirfrancisbillard@gmail.com>
2017-05-17 13:55:16 -1000

    Update ttt_banana_split.lua

--- stupid-ttt/lua/entities/ttt_banana_split.lua
+++ stupid-ttt/lua/entities/ttt_banana_split.lua
@@ -8,30 +8,33 @@ ENT.Base = "ttt_basegrenade_proj"
 
 ENT.Model = Model("models/props/cs_italy/bananna.mdl")
 
-AccessorFunc( ENT, "radius", "Radius", FORCE_NUMBER )
-AccessorFunc( ENT, "dmg", "Dmg", FORCE_NUMBER )
+AccessorFunc(ENT, "radius", "Radius", FORCE_NUMBER)
+AccessorFunc(ENT, "dmg", "Dmg", FORCE_NUMBER)
 
 function ENT:Initialize()	
-   self:SetModel(self.Model)
-   
-   if not self:GetRadius() then self:SetRadius(256) end
-   if not self:GetDmg() then self:SetDmg(25) end 
-   
-   local phys = self:GetPhysicsObject()
-   if IsValid(phys) then
-	phys:SetMass(1)
-   end
-   
-   timer.Simple(math.random(3,5), function() if IsValid(self) then self:Explode() end end)
-   
-   return self.BaseClass.Initialize(self)
+	self:SetModel(self.Model)
+
+	if not self:GetRadius() then self:SetRadius(750) end
+	if not self:GetDmg() then self:SetDmg(50) end 
+
+	local phys = self:GetPhysicsObject()
+		if IsValid(phys) then
+		phys:SetMass(1)
+	end
+
+	local temps = math.random(2, 3)
+
+	timer.Simple(temps - 1, function() if SERVER and IsValid(self) then PraiseAllah(self) end end)
+	timer.Simple(temps, function() if IsValid(self) then self:Explode() end end)
+
+	return self.BaseClass.Initialize(self)
 end
 
 function ENT:Explode()
 	if SERVER then
-		if !self:IsValid() then return end
-		local pos = self.Entity:GetPos()
-				  
+		if not IsValid(self) then return end
+		local pos = self:GetPos()
+
 		local effect = EffectData()
 		effect:SetStart(pos)
 		effect:SetOrigin(pos)
@@ -41,7 +44,7 @@ function ENT:Explode()
 
 		util.Effect("Explosion", effect, true, true)
 		util.BlastDamage(self, self.Owner, pos, self:GetRadius(), self:GetDmg())
-		
+
 		self:Remove()
 	end
 end
@@ -50,7 +53,7 @@ function ENT:PhysicsCollide(data,phys)
 	if data.Speed > 50 then
 		self:EmitSound("weapons/bananaimpact.wav")
 	end
-	
+
 	local impulse = -data.Speed * data.HitNormal * .4 + (data.OurOldVelocity * -.6)
-	phys:ApplyForceCenter(impulse*2)
+	phys:ApplyForceCenter(impulse * 2)
 end
