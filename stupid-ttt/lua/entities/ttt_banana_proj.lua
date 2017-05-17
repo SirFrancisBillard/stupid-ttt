74080ed61b0d95304a17eb77871a487d214fb3b4
Sir Francis Billard <sirfrancisbillard@gmail.com>
2017-05-17 13:58:40 -1000

    Update ttt_banana_proj.lua

--- stupid-ttt/lua/entities/ttt_banana_proj.lua
+++ stupid-ttt/lua/entities/ttt_banana_proj.lua
@@ -7,49 +7,49 @@ ENT.Base = "ttt_basegrenade_proj"
 ENT.Model = Model("models/props/cs_italy/bananna_bunch.mdl")
 ENT.Icon = "vgui/ttt/icon_cyb_bananabomb.png"
 
-AccessorFunc( ENT, "radius", "Radius", FORCE_NUMBER )
-AccessorFunc( ENT, "dmg", "Dmg", FORCE_NUMBER )
+AccessorFunc(ENT, "radius", "Radius", FORCE_NUMBER)
+AccessorFunc(ENT, "dmg", "Dmg", FORCE_NUMBER)
 
 function ENT:Initialize()	
-   if not self:GetRadius() then self:SetRadius(256) end
-   if not self:GetDmg() then self:SetDmg(100) end
+	if not self:GetRadius() then self:SetRadius(750) end
+	if not self:GetDmg() then self:SetDmg(100) end
 
-   return self.BaseClass.Initialize(self)
+	return self.BaseClass.Initialize(self)
 end
 
 function ENT:Explode(tr)
-   if SERVER then
-      self.Entity:SetNoDraw(true)
-      self.Entity:SetSolid(SOLID_NONE)
+	if SERVER then
+		self:SetNoDraw(true)
+		self:SetSolid(SOLID_NONE)
 
-      -- pull out of the surface
-      if tr.Fraction != 1.0 then
-         self.Entity:SetPos(tr.HitPos + tr.HitNormal * 0.6)
-      end
+		-- pull out of the surface
+		if tr.Fraction != 1.0 then
+		self:SetPos(tr.HitPos + tr.HitNormal * 0.6)
+	end
 
-      local pos = self.Entity:GetPos()
-	
-      util.BlastDamage(self, self:GetThrower(), pos, self:GetRadius(), self:GetDmg())
-	  
-	  -- RAIN HELL BOYS!
-	  for i = 0, math.random(8,16) do
-		
+	local pos = self:GetPos()
+
+	util.BlastDamage(self, self:GetThrower(), pos, self:GetRadius(), self:GetDmg())
+
+	-- RAIN HELL BOYS!
+	for i = 0, math.random(8, 16) do
 		local ent = ents.Create("ttt_banana_split")
-		ent:SetPos(self:GetPos() + Vector(0,0,10) + VectorRand() * 5)
+		ent:SetPos(self:GetPos() + Vector(0, 0, 10) + VectorRand() * 5)
 		ent:SetOwner(self.Owner)
-		ent:SetAngles(Angle(math.Rand(0,360),math.Rand(0,360),math.Rand(0,360)))
+		ent:SetAngles(Angle(math.Rand(0, 360), math.Rand(0, 360), math.Rand(0, 360)))
 		ent:Spawn()
 		ent:Activate()
 		
 		local phys = ent:GetPhysicsObject()
 		if IsValid(phys) then
 			local vrnd = VectorRand()
-			timer.Simple(0.1, function() 
-				phys:ApplyForceCenter((200 * vrnd * math.Rand(4,8))+Vector(0,0,100))
+			timer.Simple(0.1, function()
+				if not IsValid(phys) then return end
+				phys:ApplyForceCenter((800 * vrnd * math.Rand(4, 8)) + Vector(0, 0, 100))
 			end)
 		end
 	  end
-	  
+
 	  -- make sure we are removed, even if errors occur later
       self:Remove()
 
@@ -60,7 +60,7 @@ function ENT:Explode(tr)
       if tr.Fraction != 1.0 then
          effect:SetNormal(tr.HitNormal)
       end
-      
+
       util.Effect("Explosion", effect, true, true)
       util.Effect("cball_explode", effect, true, true)
 
@@ -77,7 +77,7 @@ function ENT:PhysicsCollide(data,phys)
 	if data.Speed > 50 then
 		self:EmitSound("weapons/bananaimpact.wav")
 	end
-	
+
 	local impulse = -data.Speed * data.HitNormal * .4 + (data.OurOldVelocity * -.6)
 	phys:ApplyForceCenter(impulse)
 end
