efbdcceb5814fcfce19efe4af8819d615b94a525
Sir Francis Billard <sirfrancisbillard@gmail.com>
2017-10-13 12:23:20 -1000

    Dumb formatting

--- stupid-ttt/lua/autorun/sh_ttt_jest.lua
+++ stupid-ttt/lua/autorun/sh_ttt_jest.lua
@@ -2,26 +2,26 @@
 AddCSLuaFile()
 
 hook.Add("DoPlayerDeath", "StupidTTT.Equipment.Jest", function(ply)
-			if IsValid(ply) and ply:IsPlayer() and ply:HasEquipmentItem(EQUIP_JEST) then
-				local pos = ply:GetPos()
-				ply:EmitSound("Jihad.Scream")
+	if IsValid(ply) and ply:IsPlayer() and ply:HasEquipmentItem(EQUIP_JEST) then
+		ply:EmitSound("Jihad.Scream")
+		local pos = ply:GetPos()
 
-				timer.Simple(1, function()
-					local boom = EffectData()
-					boom:SetOrigin(pos)
+		timer.Simple(1, function()
+			local boom = EffectData()
+			boom:SetOrigin(pos)
 
-					sound.Play(Sound("^phx/explode0" .. math.random(0, 6) .. ".wav"), pos)
+			sound.Play(Sound("^phx/explode0" .. math.random(0, 6) .. ".wav"), pos)
 
-					util.Decal("Rollermine.Crater", pos, pos - Vector(0, 0, 500), nil)
-					util.Decal("Scorch", pos, pos - Vector(0, 0, 500), nil)
+			util.Decal("Rollermine.Crater", pos, pos - Vector(0, 0, 500), nil)
+			util.Decal("Scorch", pos, pos - Vector(0, 0, 500), nil)
 
-					util.BlastDamage(ply, ply, pos, math.random(64, 256), math.random(64, 128))
+			util.BlastDamage(ply, ply, pos, math.random(64, 256), math.random(64, 128))
 
-					timer.Simple(0.5, function()
-						if not pos then return end
+			timer.Simple(0.5, function()
+				if not pos then return end
 
-						sound.Play(Sound("Jihad.Islam"), pos)
-					end)
-				end)
-			end
+				sound.Play(Sound("Jihad.Islam"), pos)
+			end)
 		end)
+	end
+end)
