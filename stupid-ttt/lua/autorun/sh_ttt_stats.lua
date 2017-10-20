c09a5d570f3f8540451a6be96a274e784c714bf6
Sir Francis Billard <sirfrancisbillard@gmail.com>
2017-10-19 14:54:28 -1000

    Global weapon stats

---
+++ stupid-ttt/lua/autorun/sh_ttt_stats.lua
@@ -0,0 +1,65 @@
+AddCSLuaFile()
+
+-- these are the stats for the default TTT weapons
+-- i put them here so they can be used to achieve proportionally balanced stats for custom weapons
+
+WeaponStats = {
+	Glock = {
+		ClipSize = 20,
+		Cone = 0.028,
+		Damage = 12,
+		Delay = 0.10,
+		Recoil = 0.9
+	},
+	M16 = {
+		ClipSize = 20,
+		Cone = 0.018,
+		Damage = 23,
+		Delay = 0.19,
+		Recoil = 1.6
+	},
+	Mac10 = {
+		ClipSize = 30,
+		Cone = 0.03,
+		Damage = 12,
+		Delay = 0.065,
+		Recoil = 1.15
+	},
+	Pistol = {
+		ClipSize = 20,
+		Cone = 0.02,
+		Damage = 25,
+		Delay = 0.38,
+		Recoil = 1.5
+	},
+	Deagle = {
+		ClipSize = 8,
+		Cone = 0.02,
+		Damage = 37,
+		Delay = 0.6,
+		Recoil = 6
+	},
+	Rifle = {
+		ClipSize = 10,
+		Cone = 0.005,
+		Damage = 50,
+		Delay = 1.5,
+		HeadshotMultiplier = 4,
+		Recoil = 7
+	},
+	Shotgun = {
+		ClipSize = 8,
+		Cone = 0.085,
+		Damage = 11,
+		Delay = 0.8,
+		NumShots = 8,
+		Recoil = 7
+	},
+	Huge = {
+		ClipSize = 150,
+		Cone = 0.09,
+		Damage = 7,
+		Delay = 0.06,
+		Recoil = 1.9
+	}
+}
